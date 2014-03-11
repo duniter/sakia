d32cb5fb08d98ab5b7a5b15c6c159652a54e2e14
Inso <insomniak.fr@gmail.com>
2014-03-11 01:02:46 +0100

    cx_freeze build to have an easy-to-deploy app

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# source d'inspiration: http://wiki.wxpython.org/cx_freeze
+
+import sys, os
+from cx_Freeze import setup, Executable
+
+#############################################################################
+# preparation des options
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+print(sys.path)
+includes = ["sip", "re", "json", "logging", "hashlib", "os", "urllib", "ucoinpy"]
+excludes = []
+packages = ["gnupg"]
+
+options = {"path": sys.path,
+           "includes": includes,
+           "excludes": excludes,
+           "packages": packages
+           }
+
+#############################################################################
+# preparation des cibles
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+target = Executable(
+    script = "src/cutecoin/__init__.py",
+    base = base,
+    compress = True,
+    icon = None,
+    )
+
+#############################################################################
+# creation du setup
+setup(
+    name = "cutecoin",
+    version = "0.3.0",
+    description = "UCoin client",
+    author = "Inso",
+    options = {"build_exe": options},
+    executables = [target]
+    )
+
