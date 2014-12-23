eb6f18fa0becd3732545710c592df58f1dace966
Inso <insomniak.fr@gmail.com>
2014-12-23 11:48:02 +0100

    Using pylibscrypt : easy to integrate in cxfreeze, fast and reliable !

--- lib/ucoinpy/key/__init__.py
+++ lib/ucoinpy/key/__init__.py
@@ -6,7 +6,7 @@ Ucoin public and private keys
 
 import base58
 import base64
-from .scrypt import hash
+from pylibscrypt import scrypt
 from libnacl.sign import Signer as NaclSigningKey
 
 
@@ -17,15 +17,22 @@ SCRYPT_PARAMS = {'N': 4096,
                  'p': 1
                  }
 
+def _ensure_bytes(data):
+    if isinstance(data, str):
+        return bytes(data, 'utf-8')
+
+    return data
 
 class SigningKey(NaclSigningKey):
     def __init__(self, salt, password):
-        seed = hash(password, salt,
+        salt = _ensure_bytes(salt)
+        password = _ensure_bytes(password)
+        seed = scrypt(password, salt,
                     SCRYPT_PARAMS['N'], SCRYPT_PARAMS['r'], SCRYPT_PARAMS['p'],
                     SEED_LENGTH)
 
         super().__init__(seed)
-        self.pubkey = Base58Encoder.encoder(self.vk)
+        self.pubkey = Base58Encoder.encode(self.vk)
 
 class Base58Encoder(object):
     @staticmethod
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 print(sys.path)
 includes = ["sip", "re", "json", "logging", "hashlib", "os", "urllib", "ucoinpy", "requests", "cutecoin.core"]
 excludes = []
-packages = ["libnacl"]
+packages = ["libnacl", "pylibscrypt"]
 includefiles = []
 
 options = {"path": sys.path,
