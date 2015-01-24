4978fcbf75d0f158e6979c138de0baadb3ba502d
Inso <insomniak.fr@gmail.com>
2015-01-24 18:02:20 +0100

    Fixed wallet refreshing and removed double decimals for transfer

--- src/cutecoin/models/wallets.py
+++ src/cutecoin/models/wallets.py
@@ -21,17 +21,15 @@ class WalletsListModel(QAbstractListModel):
         super(WalletsListModel, self).__init__(parent)
         self.wallets = account.wallets
         self.community = community
-        self.values = []
-        for w in self.wallets:
-            self.values.append(w.get_text(self.community))
 
     def rowCount(self, parent):
         return len(self.wallets)
 
     def data(self, index, role):
         row = index.row()
+        w = self.wallets[row]
         if role == Qt.DisplayRole:
-            return self.values[row]
+            return w.get_text(self.community)
         elif role == Qt.EditRole:
             return self.wallets[row].name
 
@@ -39,7 +37,6 @@ class WalletsListModel(QAbstractListModel):
         if role == Qt.EditRole:
             row = index.row()
             self.wallets[row].name = value
-            self.values[row] = self.wallets[row].get_text()
             self.dataChanged.emit(index, index)
             return True
         return False
