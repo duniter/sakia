c99ea1c24890aa10576f12fc609a3427ca892098
Inso <insomniak.fr@gmail.com>
2015-05-03 16:37:40 +0200

    Removing old call to set Github CA

--- src/cutecoin/main.py
+++ src/cutecoin/main.py
@@ -15,17 +15,6 @@ from cutecoin.core.app import Application
 if __name__ == '__main__':
     # activate ctrl-c interrupt
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    if getattr(sys, 'frozen', False):
-        # The application is frozen
-        datadir = os.path.dirname(sys.executable)
-        os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(datadir, "DigiCertHighAssuranceEVRootCA.crt")
-    else:
-        # The application is not frozen
-        # Change this bit to match where you store your data files:
-        datadir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(datadir,
-                                                        "res", "certs",
-                                                        "DigiCertHighAssuranceEVRootCA.crt")
 
     cutecoin = QApplication(sys.argv)
     app = Application(sys.argv)
