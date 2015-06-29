01734a08926205361b53073684bd06f30050926d
Inso <insomniak.fr@gmail.com>
2015-06-29 12:35:11 +0200

    Handling exceptions

--- src/cutecoin/main.py
+++ src/cutecoin/main.py
@@ -5,7 +5,6 @@ Created on 1 févr. 2014
 """
 import signal
 import sys
-import os
 import logging
 import asyncio
 
@@ -13,6 +12,11 @@ from quamash import QEventLoop
 from PyQt5.QtWidgets import QApplication
 from cutecoin.gui.mainwindow import MainWindow
 from cutecoin.core.app import Application
+
+def handle_exception(loop, context):
+    logging.debug("Async exception : raise {1} : {0} in {2}".format(context['message'],
+                                              str(context['exception']),
+                                              str(context['future'])))
 
 if __name__ == '__main__':
     # activate ctrl-c interrupt
@@ -22,6 +26,7 @@ if __name__ == '__main__':
     loop = QEventLoop(cutecoin)
     app = Application(sys.argv, cutecoin, loop)
     asyncio.set_event_loop(loop)
+    loop.set_exception_handler(handle_exception)
     with loop:
         window = MainWindow(app)
         window.showMaximized()
