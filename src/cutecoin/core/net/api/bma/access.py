53addd1091d1aed719b48fcae89221812d38dff7
Inso <insomniak.fr@gmail.com>
2015-08-19 13:15:23 +0200

    Add block hash to cached metadata

--- src/cutecoin/core/net/api/bma/access.py
+++ src/cutecoin/core/net/api/bma/access.py
@@ -106,7 +106,7 @@ class BmaAccess(QObject):
             cached_data = self._data[cache_key]
             need_reload = True
             if str(request) in BmaAccess.__saved_requests \
-                or cached_data['metadata']['block'] >= self._network.latest_block_number:
+                or cached_data['metadata']['block_hash'] == self._network.latest_block_hash:
                 need_reload = False
             ret_data = cached_data['value']
         else:
@@ -130,7 +130,8 @@ class BmaAccess(QObject):
                                      'value': {}}
 
         if not self._compare_json(self._data[cache_key]['value'], data):
-            self._data[cache_key]['metadata']['block'] = self._network.latest_block_number
+            self._data[cache_key]['metadata']['block_number'] = self._network.latest_block_number
+            self._data[cache_key]['metadata']['block_hash'] = self._network.latest_block_hash
             self._data[cache_key]['metadata']['cutecoin_version'] = __version__
             self._data[cache_key]['value'] = data
             return True
