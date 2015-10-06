71f7707c5bbfb01f4b5a968b3ec50f2838d5aa86
Inso <insomniak.fr@gmail.com>
2015-10-06 08:52:55 +0200

    Remove lib dir and install ucoinpy from pip

--- lib/ucoinpy/documents/__init__.py
+++
@@ -1,7 +0,0 @@
-from .block import Block, BlockId
-from .certification import SelfCertification, Certification
-from .membership import Membership
-from .peer import Endpoint, BMAEndpoint, UnknownEndpoint, Peer
-from .status import Status
-from .transaction import SimpleTransaction, Transaction
-from .document import Document
