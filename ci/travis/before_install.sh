022cd0dbb9d1ff1a840c87ecb51a9b81dc390d6a
Inso <insomniak.fr@gmail.com>
2015-12-18 19:30:16 +0100

    Cache dependencies

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -9,29 +9,32 @@ brew install pyenv-virtualenv
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.5.0
-
-pyenv shell 3.5.0
-pyenv virtualenv sakia-env
-
-cd $HOME
-
-wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
-tar xzf sip-4.17.tar.gz
-cd sip-4.17/
 pyenv activate sakia-env
-python configure.py
-make && make install
-pyenv rehash
+if [ ] then
+    env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $PYENV_PYTHON_VERSION
 
-cd $HOME
+    pyenv shell $PYENV_PYTHON_VERSION
+    pyenv virtualenv sakia-env
 
-wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
-tar xzf PyQt-gpl-5.5.1.tar.gz
-cd PyQt-gpl-5.5.1/
-pyenv activate sakia-env
-python configure.py --verbose --confirm-license
-make && make install
-pyenv rehash
+    cd $HOME
 
-cd $HOME/sakia
+    wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
+    tar xzf sip-4.17.tar.gz
+    cd sip-4.17/
+    pyenv activate sakia-env
+    python configure.py
+    make && make install
+    pyenv rehash
+
+    cd $HOME
+
+    wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
+    tar xzf PyQt-gpl-5.5.1.tar.gz
+    cd PyQt-gpl-5.5.1/
+    pyenv activate sakia-env
+    python configure.py --verbose --confirm-license
+    make && make install
+    pyenv rehash
+
+    cd $HOME/sakia
+fi
