83dc14463183562a36c50059763a938d18c997a3
Inso <insomniak.fr@gmail.com>
2015-12-18 18:57:37 +0100

    Try to build on travis for osx

---
+++ ci/travis/before_install.sh
@@ -0,0 +1,33 @@
+brew install wget
+brew install libsodium
+## Ensure your brew QT version is up to date. (brew install qt -> qt 4.8)
+brew install qt5
+brew link --force qt5
+
+
+env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $TRAVIS_PYTHON_VERSION
+
+pyenv shell 3.5.0
+pyenv virtualenv sakia-env
+
+cd $HOME
+
+wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
+tar xzf sip-4.17.tar.gz
+cd sip-4.17/
+pyenv activate sakia-env
+python configure.py
+make && make install
+pyenv rehash
+
+cd $HOME
+
+wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
+tar xzf PyQt-gpl-5.5.1.tar.gz
+cd PyQt-gpl-5.5.1/
+pyenv activate sakia-env
+python configure.py --verbose
+make && make install
+pyenv rehash
+
+cd $HOME/sakia
