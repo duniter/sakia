e37f0b672f532c770923ec09ad6ebc71a52506a8
Inso <insomniak.fr@gmail.com>
2015-12-19 15:28:21 +0100

    Enhance test script

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -6,5 +6,10 @@ eval "$(pyenv virtualenv-init -)"
 cd $HOME/build/ucoin-io/sakia
 pyenv activate sakia-env
 
+export XVFBARGS="-screen 0 1280x1024x24"
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+sleep 3
+
 coverage run --source=sakia.core,sakia.gui,sakia.models run_tests.py
 
