5a1153d3b395e864a257585f2b9633b939730f28
Inso <insomniak.fr@gmail.com>
2015-12-19 09:01:08 +0100

    Add missing file

---
+++ ci/travis/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+cd $HOME/build/ucoin-io/sakia
+pyenv activate sakia-env
+
+coverage run --source=sakia.core,sakia.gui,sakia.models run_tests.py
+
