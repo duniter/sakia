a252de97a28905c095605a731a2f30a809bb6ece
Inso <insomniak.fr@gmail.com>
2015-12-19 20:40:15 +0100

    With notify2

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -8,6 +8,12 @@ pyenv activate sakia-env
 pip install coveralls
 pip install cx_Freeze
 pip install -r requirements.txt
+if [ $TRAVIS_OS_NAME == "linux" ]
+then
+    pip install -U git+https://github.com/posborne/dbus-python.git
+    pip install notify2
+fi
+
 python gen_resources.py
 python gen_translations.py
 
