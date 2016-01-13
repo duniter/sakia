01465a375f9569989b5b3265c915bd637b89812e
Inso <insomniak.fr@gmail.com>
2016-01-13 07:18:28 +0100

    Implementation of past ud in tx history

--- src/sakia/core/money/__init__.py
+++ src/sakia/core/money/__init__.py
@@ -4,4 +4,4 @@ from .quant_zerosum import QuantitativeZSum
 from .relative_zerosum import RelativeZSum
 from .relative_to_past import RelativeToPast
 
-Referentials = (Quantitative, Relative, QuantitativeZSum, RelativeZSum)
+Referentials = (Quantitative, Relative, QuantitativeZSum, RelativeZSum, RelativeToPast)
