13b351616719ca3a6ba2fa08f1c8446b38aeb755
Inso <insomniak.fr@gmail.com>
2016-02-01 22:04:51 +0100

    Add hook for missing platformthemes on linux

---
+++ hooks/hook-PyQt5.QtGui.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2016, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License with exception
+# for distributing bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+hiddenimports = ['sip', 'PyQt5.QtCore']
+
+from PyInstaller.utils.hooks import qt5_plugins_binaries
+from PyInstaller.compat import is_linux
+
+
+binaries = []
+binaries.extend(qt5_plugins_binaries('accessible'))
+binaries.extend(qt5_plugins_binaries('iconengines'))
+binaries.extend(qt5_plugins_binaries('imageformats'))
+binaries.extend(qt5_plugins_binaries('inputmethods'))
+binaries.extend(qt5_plugins_binaries('graphicssystems'))
+binaries.extend(qt5_plugins_binaries('platforms'))
+
+if is_linux:
+    binaries.extend(qt5_plugins_binaries('platformthemes'))
