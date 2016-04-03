b9d323ba90b27534e78877d1c7091a2b0ac5369f
inso <insomniak.fr@gmaiL.com>
2016-04-03 20:28:05 +0200

    Fix travis build

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -11,13 +11,15 @@ then
     brew install pyenv-virtualenv
 elif [ $TRAVIS_OS_NAME == "linux" ]
 then
+    wget https://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
+    chmod +x qt-unified-linux-x64-online.run
+    ./qt-unified-linux-x64-online.run --script qt-installer-non-interactive.qs
     sudo apt-get update
     sudo apt-get install -qq -y libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 \
             libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev \
             libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0 \
             libxcb-render-util0-dev libxcb-glx0-dev libgl1-mesa-dri libegl1-mesa libpcre3-dev \
-            curl qt5-qmake qtbase5-dev qttools5-dev-tools libqt5svg5-dev libqt5webkit5-dev \
-            libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool
+            curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool
     wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium13_1.0.1-1_amd64.deb
     sudo dpkg -i libsodium13_1.0.1-1_amd64.deb
     curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
@@ -65,15 +67,17 @@ then
             --enable QtGui \
             --enable QtSvg \
             --enable QtWebKitWidgets \
+            --enable QtNetwork \
             --enable QtTest
     elif [ $TRAVIS_OS_NAME == "linux" ]
     then
-        python configure.py --qmake "/usr/lib/x86_64-linux-gnu/qt5/bin/qmake" --confirm-license  \
+        python configure.py --qmake "/tmp/qt/5.5/5.5/gcc_64/bin/qmake" --confirm-license  \
             --enable QtCore \
             --enable QtWidgets \
             --enable QtGui \
             --enable QtSvg\
             --enable QtWebKitWidgets \
+            --enable QtNetwork \
             --enable QtTest
     fi
 
