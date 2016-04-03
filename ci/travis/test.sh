0c51f44b9f5ba374da42dfb399370fe60176401a
inso <insomniak.fr@gmaiL.com>
2016-04-03 23:25:55 +0200

    Restart xvfb before tests

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -3,6 +3,14 @@
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+if [ $TRAVIS_OS_NAME == "linux" ]
+then
+    export XVFBARGS="-screen 0 1280x1024x24"
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb restart
+    sleep 3
+fi
+
 cd $HOME/build/ucoin-io/sakia
 pyenv activate sakia-env
 coverage run --source=sakia.core,sakia.gui,sakia.models setup.py test
