b2fa6f0ade59e7bc5682ec2c73975603714b4ec9
inso <insomniak.fr@gmaiL.com>
2016-04-03 22:18:14 +0200

    Script filename

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -20,15 +20,17 @@ then
     brew install pyenv-virtualenv
 elif [ $TRAVIS_OS_NAME == "linux" ]
 then
-    wget https://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
-    chmod +x qt-opensource-linux-x64-5.5.1.run
-    ./qt-opensource-linux-x64-5.5.1.run --script $HOME/build/ucoin-io/sakia/ci/travis/qt-installer-non-interactive.qs
     sudo apt-get update
     sudo apt-get install -qq -y libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 \
             libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev \
             libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0 \
             libxcb-render-util0-dev libxcb-glx0-dev libgl1-mesa-dri libegl1-mesa libpcre3-dev \
-            curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool
+            curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool libssl
+
+    wget https://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
+    chmod +x qt-opensource-linux-x64-5.5.1.run
+    ./qt-opensource-linux-x64-5.5.1.run --script $HOME/build/ucoin-io/sakia/ci/travis/qt-installer-noninteractive.qs
+
     wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium13_1.0.1-1_amd64.deb
     sudo dpkg -i libsodium13_1.0.1-1_amd64.deb
     curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
