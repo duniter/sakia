6e98079035ed8ecca4221da95cafebdcd46d289a
inso <insomniak.fr@gmaiL.com>
2016-04-30 19:32:45 +0200

    Install with wheels

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -27,9 +27,9 @@ then
             libxcb-render-util0-dev libxcb-glx0-dev libgl1-mesa-dri libegl1-mesa libpcre3-dev \
             curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool libgstreamer-plugins-base0.10-0
 
-    wget https://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
-    chmod +x qt-opensource-linux-x64-5.5.1.run
-    ./qt-opensource-linux-x64-5.5.1.run --script $HOME/build/duniter/sakia/ci/travis/qt-installer-noninteractive.qs
+    wget https://download.qt.io/official_releases/qt/5.6/5.6.0/qt-opensource-linux-x64-5.6.0.run
+    chmod +x qt-opensource-linux-x64-5.6.0.run
+    ./qt-opensource-linux-x64-5.6.0.run --script $HOME/build/duniter/sakia/ci/travis/qt-installer-noninteractive.qs
 
     wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium13_1.0.1-1_amd64.deb
     sudo dpkg -i libsodium13_1.0.1-1_amd64.deb
@@ -50,53 +50,5 @@ then
     then
         PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYENV_PYTHON_VERSION
     fi
-
-    pyenv shell $PYENV_PYTHON_VERSION
-    pyenv virtualenv sakia-env
-
-    cd $HOME
-
-    wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
-    tar xzf sip-4.17.tar.gz
-    cd sip-4.17/
-    pyenv activate sakia-env
-    python configure.py
-    make && make install
-    pyenv rehash
-
-    cd $HOME
-
-    wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
-    tar xzf PyQt-gpl-5.5.1.tar.gz
-    cd PyQt-gpl-5.5.1/
-    pyenv activate sakia-env
-    if [ $TRAVIS_OS_NAME == "osx" ]
-    then
-        python configure.py --confirm-license \
-            --enable QtCore \
-            --enable QtWidgets \
-            --enable QtGui \
-            --enable QtSvg \
-            --enable QtWebChannel \
-            --enable QtWebEngineWidgets \
-            --enable QtNetwork \
-            --enable QtPrintSupport \
-            --enable QtTest
-    elif [ $TRAVIS_OS_NAME == "linux" ]
-    then
-        python configure.py --qmake "/tmp/qt/5.5/5.5/gcc_64/bin/qmake" --confirm-license  \
-            --enable QtCore \
-            --enable QtWidgets \
-            --enable QtGui \
-            --enable QtSvg \
-            --enable QtWebChannel \
-            --enable QtWebEngineWidgets \
-            --enable QtNetwork \
-            --enable QtPrintSupport \
-            --enable QtTest
-    fi
-
-    make -j 2 && make install
-    pyenv rehash
 
 fi
