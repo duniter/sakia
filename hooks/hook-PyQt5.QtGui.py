d12f55f446b10964284471e8a68fea95e6db0360
inso <insomniak.fr@gmaiL.com>
2016-05-04 07:34:53 +0200

    Fix hook

--- hooks/hook-PyQt5.QtGui.py
+++ hooks/hook-PyQt5.QtGui.py
@@ -15,12 +15,12 @@ from PyInstaller.compat import is_linux
 
 
 binaries = []
-binaries.extend(qt_plugins_binaries('accessible'))
-binaries.extend(qt_plugins_binaries('iconengines'))
-binaries.extend(qt_plugins_binaries('imageformats'))
-binaries.extend(qt_plugins_binaries('inputmethods'))
-binaries.extend(qt_plugins_binaries('graphicssystems'))
-binaries.extend(qt_plugins_binaries('platforms'))
+binaries.extend(qt_plugins_binaries('accessible', namespace='PyQt5'))
+binaries.extend(qt_plugins_binaries('iconengines', namespace='PyQt5'))
+binaries.extend(qt_plugins_binaries('imageformats', namespace='PyQt5'))
+binaries.extend(qt_plugins_binaries('inputmethods', namespace='PyQt5'))
+binaries.extend(qt_plugins_binaries('graphicssystems', namespace='PyQt5'))
+binaries.extend(qt_plugins_binaries('platforms', namespace='PyQt5'))
 
 if is_linux:
-    binaries.extend(qt_plugins_binaries('platformthemes'))
+    binaries.extend(qt_plugins_binaries('platformthemes', namespace='PyQt5'))
