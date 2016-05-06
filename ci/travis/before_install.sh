0e3acc8a3b91a7e0804dbd57a079a2c5d37385dc
inso <insomniak.fr@gmaiL.com>
2016-05-06 13:38:28 +0200

    ldconfig

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -26,7 +26,6 @@ then
             libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0 \
             libxcb-render-util0-dev libxcb-glx0-dev libgl1-mesa-dri libegl1-mesa libpcre3-dev \
             curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool libgstreamer-plugins-base0.10-0
-
     wget https://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
     chmod +x qt-opensource-linux-x64-5.5.1.run
     ./qt-opensource-linux-x64-5.5.1.run --script $HOME/build/duniter/sakia/ci/travis/qt-installer-noninteractive.qs
@@ -34,6 +33,8 @@ then
     wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium13_1.0.1-1_amd64.deb
     sudo dpkg -i libsodium13_1.0.1-1_amd64.deb
     curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+    sudo lconfig
+    ldconfig -p
 fi
 
 eval "$(pyenv init -)"
