7a7309d490641ff90c7cfba9d11ce0f35bf2ab11
inso <insomniak.fr@gmaiL.com>
2016-05-06 15:04:48 +0200

    Executable mode

--- release.sh
+++ release.sh
@@ -4,11 +4,11 @@
 current=`grep -P "__version_info__ = \(\'\d+\', \'\d+\', \'\d+(\w*)\'\)" src/sakia/__init__.py | grep -oP "\'\d+\', \'\d+\', \'\d+(\w*)\'"`
 echo "Current version: $current"
 
-if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9a-z]+$ ]]; then
+if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]+$ ]]; then
   IFS='.' read -r -a array <<< "$1"
   sed -i "s/__version_info__\ = ($current)/__version_info__ = ('${array[0]}', '${array[1]}', '${array[2]}')/g" src/sakia/__init__.py
-  git commit src/sakia/__init__.py -m "$2"
-  git tag "$2"
+  git commit src/sakia/__init__.py -m "$1"
+  git tag "$1"
 else
   echo "Wrong version format"
 fi
