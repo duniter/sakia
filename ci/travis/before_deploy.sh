c929e0db915d545cbf1ec5e798bffb665619bf29
cgeek <cem.moreau@gmail.com>
2016-05-15 17:31:40 +0200

    Add debian packaging

--- ci/travis/before_deploy.sh
+++ ci/travis/before_deploy.sh
@@ -6,4 +6,15 @@ then
 elif [ $TRAVIS_OS_NAME == "linux" ]
 then
     zip -r sakia-${TRAVIS_OS_NAME}.zip dist/
+
+    # Debian package
+    chmod 755 ci/travis/debian/DEBIAN/post*
+    chmod 755 ci/travis/debian/DEBIAN/pre*
+    mkdir -p ci/travis/debian/opt/sakia
+
+    cp sakia.png ci/travis/debian/opt/sakia/
+    cp sakia-${TRAVIS_OS_NAME}.zip ci/travis/debian/opt/sakia/sakia.zip
+
+    fakeroot dpkg-deb --build ci/travis/debian
+    mv ci/travis/debian.deb sakia-${TRAVIS_OS_NAME}.deb
 fi
--- release.sh
+++ release.sh
@@ -7,7 +7,8 @@ echo "Current version: $current"
 if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]+$ ]]; then
   IFS='.' read -r -a array <<< "$1"
   sed -i "s/__version_info__\ = ($current)/__version_info__ = ('${array[0]}', '${array[1]}', '${array[2]}')/g" src/sakia/__init__.py
-  sed -i "#define MyAppVerStr \"($current)\"/#define MyAppVerStr \"$1\"/
+  sed -i "s/#define MyAppVerStr .*/#define MyAppVerStr \"$1\"/g" ci/appveyor/sakia.iss
+  sed -i "s/Version: .*/Version: $1/g" ci/travis/debian/DEBIAN/control
   git commit src/sakia/__init__.py -m "$1"
   git tag "$1"
 else
