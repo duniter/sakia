9a5100ba9f16e08515ec1d7dbd985ac6f0ca4f8d
Insoleet <insomniak.fr@gmail.com>
2016-07-13 12:10:10 +0200

    Test downloaded file integrity

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -58,6 +58,8 @@ then
     cd $HOME
 
     wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
+    file sip-4.17.tar.gz
+    gzip -t sip-4.17.tar.gz
     tar xzf sip-4.17.tar.gz
     cd sip-4.17/
     pyenv activate sakia-env
@@ -68,6 +70,8 @@ then
     cd $HOME
 
     wget http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz
+    file PyQt-gpl-5.5.1.tar.gz
+    gzip -t PyQt-gpl-5.5.1.tar.gz
     tar xzf PyQt-gpl-5.5.1.tar.gz
     cd PyQt-gpl-5.5.1/
     pyenv activate sakia-env
