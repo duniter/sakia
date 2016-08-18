db196c74a106da4891b4885c147186c924f419e8
inso <insomniak.fr@gmaiL.com>
2016-08-18 18:15:53 +0200

    New revocation component

---
+++ src/sakia/gui/dialogs/revocation/controller.py
@@ -0,0 +1,132 @@
+from sakia.gui.component.controller import ComponentController
+from .view import RevocationView
+from .model import RevocationModel
+from duniterpy.documents import MalformedDocumentError
+from sakia.tools.decorators import asyncify
+import asyncio
+
+
+class RevocationController(ComponentController):
+    """
+    The revocation view
+    """
+
+    def __init__(self, parent, view, model):
+        """
+        Constructor of the revocation component
+
+        :param sakia.gui.revocation.view.revocationView: the view
+        :param sakia.gui.revocation.model.revocationModel model: the model
+        """
+        super().__init__(parent, view, model)
+
+        self.handle_next_step(init=True)
+        self.view.button_next.clicked.connect(lambda checked: self.handle_next_step(False))
+        self._steps = (
+            {
+                'page': self.ui.page_load_file,
+                'init': self.init_dialog,
+                'next': self.revocation_selected
+            },
+            {
+                'page': self.ui.page_destination,
+                'init': self.init_publication_page,
+                'next': self.publish
+            }
+        )
+        self._current_step = 0
+
+    @classmethod
+    def create(cls, parent, app, **kwargs):
+        """
+        Instanciate a revocation component
+        :param sakia.gui.component.controller.ComponentController parent:
+        :param sakia.core.Application app:
+        :return: a new revocation controller
+        :rtype: revocationController
+        """
+        account = kwargs['account']
+        view = RevocationView(parent.view)
+        model = RevocationModel(None, app, account)
+        revocation = cls(parent, view, model)
+        model.setParent(revocation)
+        return revocation
+
+    @classmethod
+    def open_dialog(cls, parent, app, account):
+        """
+        Certify and identity
+        :param sakia.gui.component.controller.ComponentController parent: the parent
+        :param sakia.core.Application app: the application
+        :param sakia.core.Account account: the account certifying the identity
+        :return:
+        """
+        dialog = cls.create(parent, app, account=account)
+        dialog.refresh()
+        return dialog.exec()
+
+    @property
+    def view(self) -> RevocationView:
+        return self._view
+
+    @property
+    def model(self) -> RevocationModel:
+        return self._model
+
+    def handle_next_step(self, init=False):
+        if self._current_step < len(self._steps) - 1:
+            if not init:
+                self.view.button_next.clicked.disconnect(self._steps[self._current_step]['next'])
+                self._current_step += 1
+            self._steps[self._current_step]['init']()
+            self.view.stackedWidget.setCurrentWidget(self._steps[self._current_step]['page'])
+            self.view.button_next.clicked.connect(self._steps[self._current_step]['next'])
+
+    def load_from_file(self):
+        selected_file = self.view.select_revocation_file()
+        try:
+            self.model.load_revocation(selected_file)
+        except FileNotFoundError:
+            pass
+        except MalformedDocumentError:
+            self.view.malformed_file_error()
+            self.button_next.setEnabled(False)
+
+    def revocation_selected(self):
+        pass
+
+    def init_publication_page(self):
+        communities_names = self.model.communities_names()
+        self.view.set_communities_names(communities_names)
+
+    def publish(self):
+        self.view.button_next.setEnabled(False)
+        if self.view.ask_for_confirmation():
+            self.accept()
+        else:
+            self.view.button_next.setEnabled(True)
+
+    @asyncify
+    async def accept(self):
+        if self.ui.radio_community.isChecked():
+            index = self.view.combo_community.currentIndex()
+            result, error = await self.model.send_to_community(index)
+        else:
+            server = self.ui.edit_address.text()
+            port = self.ui.spinbox_port.value()
+            result, error = await self.model.send_to_node(server, port)
+
+        if result:
+            self.view.accept()
+        else:
+            await self.view.revocation_broadcast_error(error)
+
+    def async_exec(self):
+        future = asyncio.Future()
+        self.widget.finished.connect(lambda r: future.set_result(r))
+        self.widget.open()
+        self.refresh()
+        return future
+
+    def exec(self):
+        self.widget.exec()
---
+++ src/sakia/gui/dialogs/revocation/model.py
@@ -0,0 +1,67 @@
+from sakia.gui.component.model import ComponentModel
+from duniterpy.documents.certification import Revocation
+from duniterpy.api import bma, errors
+from sakia.core.net import Node
+import aiohttp
+
+
+class RevocationModel(ComponentModel):
+    """
+    The model of HomeScreen component
+    """
+
+    def __init__(self, parent, app, account):
+        super().__init__(parent)
+        self.app = app
+        self.account = account
+
+        self.revocation_document = None
+        self.revoked_selfcert = None
+
+    def load_revocation(self, path):
+        """
+        Load a revocation document from a file
+        :param str path:
+        """
+        with open(path, 'r') as file:
+            file_content = file.read()
+            self.revocation_document = Revocation.from_signed_raw(file_content)
+            self.revoked_selfcert = Revocation.extract_self_cert(file_content)
+
+    def communities_names(self):
+        return [c.name for c in self.account.communities]
+
+    async def send_to_community(self, index):
+        community = self.account.communities[index]
+        responses = await community.bma_access.broadcast(bma.wot.Revoke, {},
+                                       {
+                                           'revocation': self.revocation_document.signed_raw(
+                                               self.revoked_selfcert)
+                                       })
+
+        result = False, ""
+        for r in responses:
+            if r.status == 200:
+                result = True, (await r.json())
+            elif not result[0]:
+                result = False, (await r.text())
+            else:
+                await r.release()
+
+        return result
+
+    async def send_to_node(self, server, port):
+        session = aiohttp.ClientSession()
+        try:
+            node = await Node.from_address(None, server, port, session=session)
+            conn_handler = node.endpoint.conn_handler()
+            await bma.wot.Revoke(conn_handler).post(session,
+                                                    revocation=self.revocation_document.signed_raw(
+                                                        self.revoked_selfcert))
+        except (ValueError, errors.DuniterError,
+                aiohttp.errors.ClientError, aiohttp.errors.DisconnectedError,
+                aiohttp.errors.TimeoutError) as e:
+            return False, str(e)
+        finally:
+            session.close()
+        return True, ""
