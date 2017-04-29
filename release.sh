623a97e4c19b746ced78a64e17b8286262e1f16f
inso <insomniak.fr@gmaiL.com>
2017-04-29 10:45:29 +0200

    Release.sh script update

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 current=`grep -P "__version_info__ = \(\'\d+\', \'\d+\', \'\d+(\w*)\'\)" src/sakia/__init__.py | grep -oP "\'\d+\', \'\d+\', \'\d+(\w*)\'"`
 echo "Current version: $current"
 
-if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]*$ ]]; then
+if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9A-Za-z]*$ ]]; then
   IFS='.' read -r -a array <<< "$1"
   sed -i "s/__version_info__\ = ($current)/__version_info__ = ('${array[0]}', '${array[1]}', '${array[2]}')/g" src/sakia/__init__.py
   sed -i "s/#define MyAppVerStr .*/#define MyAppVerStr \"$1\"/g" ci/appveyor/sakia.iss
