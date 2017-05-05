73683d9cf6ee159e66c2ca25b3bcdf29e0bad22b
inso <insomniak.fr@gmaiL.com>
2017-05-05 09:04:14 +0200

    Fix path of desktop file

--- release.sh
+++ release.sh
@@ -9,8 +9,8 @@ if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9A-Za-z]*$ ]]; then
   sed -i "s/__version_info__\ = ($current)/__version_info__ = ('${array[0]}', '${array[1]}', '${array[2]}')/g" src/sakia/__init__.py
   sed -i "s/#define MyAppVerStr .*/#define MyAppVerStr \"$1\"/g" ci/appveyor/sakia.iss
   sed -i "s/Version: .*/Version: $1/g" ci/travis/debian/DEBIAN/control
-  sed -i "s/Version=.*/Version=$1/g" ci/travis/debian/usr/share/applications/sakia.desktop
-  git commit src/sakia/__init__.py ci/appveyor/sakia.iss ci/travis/debian/DEBIAN/control ci/travis/debian/usr/share/applications/sakia.desktop -m "$1"
+  sed -i "s/Version=.*/Version=$1/g" res/linux/usr/share/applications/sakia.desktop
+  git commit src/sakia/__init__.py ci/appveyor/sakia.iss ci/travis/debian/DEBIAN/control res/linux/usr/share/applications/sakia.desktop -m "$1"
   git tag "$1" -a -m "$1"
 else
   echo "Wrong version format"
