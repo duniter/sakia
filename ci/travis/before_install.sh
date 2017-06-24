bf62dcbe5c2b16341ffbbf7a783432619706cab6
inso <insomniak.fr@gmaiL.com>
2017-06-24 13:54:28 +0200

    Install of fakeroot

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -27,7 +27,7 @@ then
             libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0 \
             libxcb-render-util0-dev libxcb-glx0-dev libgl1-mesa-dri libegl1-mesa libpcre3 libgles2-mesa-dev \
             freeglut3-dev libfreetype6-dev xorg-dev xserver-xorg-input-void xserver-xorg-video-dummy xpra libosmesa6-dev \
-            curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool libgstreamer-plugins-base0.10-0 dunst
+            curl libdbus-1-dev libdbus-glib-1-dev autoconf automake libtool libgstreamer-plugins-base0.10-0 dunst fakeroot
     wget https://download.qt.io/official_releases/qt/5.8/5.8.0/qt-opensource-linux-x64-5.8.0.run
     chmod +x qt-opensource-linux-x64-5.8.0.run
     ./qt-opensource-linux-x64-5.8.0.run --script $HOME/build/duniter/sakia/ci/travis/qt-installer-noninteractive.qs
