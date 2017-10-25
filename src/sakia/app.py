c68b7b3abd8e770229e9778020c9e0a6720c9f20
inso <insomniak.fr@gmaiL.com>
2017-10-25 06:34:09 +0200

    Fix error #754

--- src/sakia/app.py
+++ src/sakia/app.py
@@ -216,7 +216,7 @@ class Application(QObject):
                         logging.debug("Found version : {0}".format(latest_version))
                         logging.debug("Current version : {0}".format(__version__))
                         self.available_version = version
-        except (aiohttp.errors.ClientError, aiohttp.errors.ServerDisconnectedError, asyncio.TimeoutError) as e:
+        except (aiohttp.ClientError, aiohttp.ServerDisconnectedError, asyncio.TimeoutError) as e:
             self._logger.debug("Could not connect to github : {0}".format(str(e)))
 
     def save_parameters(self, parameters):
