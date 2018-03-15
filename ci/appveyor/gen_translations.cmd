26efda3b85f731a99590c976543b29ff9a4813fa
inso <insomniak.fr@gmaiL.com>
2018-03-15 13:31:58 +0100

    Fix path

--- ci/appveyor/gen_translations.cmd
+++ ci/appveyor/gen_translations.cmd
@@ -1,8 +1,12 @@
 @ECHO ON
 
+SET PREVPATH=%PATH%
 SET PATH=%QTDIR%\\bin;%QTDIR%\\lib;%PATH%
 
 lrelease -version
 
 python gen_translations.py
 if %errorlevel% neq 0 exit /b 1
+
+SET PATH=%PREVPATH%
+
