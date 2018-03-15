c7f5cf4cdfc85521fd035131b7b69a44cdee5831
inso <insomniak.fr@gmaiL.com>
2018-03-15 15:30:22 +0100

    Path to Qt bin

--- ci/appveyor/build.cmd
+++ ci/appveyor/build.cmd
@@ -22,7 +22,7 @@ if %errorlevel% neq 0 exit /b 1s
 call .\\ci\\appveyor\\gen_translations.cmd
 if %errorlevel% neq 0 exit /b 1
 
-SET PATH=c:\\python35_64\\envs\\test-environment\\lib\\site-packages\\PyQt5\\;%PATH%
+SET PATH=c:\\python35_64\\envs\\test-environment\\lib\\site-packages\\PyQt5\\Qt\\bin;%PATH%
 
 pyinstaller sakia.spec
 if %errorlevel% neq 0 exit /b 1
